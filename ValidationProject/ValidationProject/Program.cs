537778d6ae41d706f094f2102d1923db8a26424f
SOFTSERVE\ikopy <ikopy@softserveinc.com>
2023-06-26 02:36:42 +0300

    UC#10 Infrastructure setup

---
+++ ValidationProject/ValidationProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ValidationProject
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
